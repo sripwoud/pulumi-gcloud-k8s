e44e2d18819a70d2dec0f4cc4eec706b6e6eb4b8
3pwd <me@3pwd.xyz>
2023-04-25 13:01:17 +0200

    use a pulumi provider instance to access k8s cluster
    Instead of the kubeconfig output locally exported

--- resources/service.ts
+++ resources/service.ts
@@ -1,13 +1,21 @@
 import * as k8s from '@pulumi/kubernetes'
+import { namespace, provider } from './cluster'
 import { isMinikube } from './config'
-import { appLabels, appName, deployment } from './deployment'
+import { appName, deployment, labels } from './deployment'
 
 // Allocate an IP to the Deployment.
-export const frontend = new k8s.core.v1.Service(appName, {
-  metadata: { labels: deployment.spec.template.metadata.labels },
-  spec: {
-    ports: [{ port: 80, protocol: 'TCP', targetPort: 80 }],
-    selector: appLabels,
-    type: isMinikube ? 'ClusterIP' : 'LoadBalancer',
+export const frontend = new k8s.core.v1.Service(
+  appName,
+  {
+    metadata: {
+      labels: deployment.spec.template.metadata.labels,
+      namespace,
+    },
+    spec: {
+      ports: [{ port: 80, protocol: 'TCP', targetPort: 80 }],
+      selector: labels,
+      type: isMinikube ? 'ClusterIP' : 'LoadBalancer',
+    },
   },
-})
+  { provider },
+)
